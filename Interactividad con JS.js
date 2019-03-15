f5246fccbf55d4d58ed64ab4bdbf415fb973f820
Hector-HG <Hector-HG.github.com>
2019-03-15 00:39:33 -0600

    Update Interactividad con JS.js

--- Interactividad con JS.js
+++ Interactividad con JS.js
@@ -2,17 +2,15 @@ const prime_numbers = 20;
 const max = 1000;
 let _number = prompt("Schreiben Sie eine Zahl, die gleich 20 oder größer ist",20);
 let number = Number(_number);
+
 if (number>1000) {
 	alert("Es ist viele, besser 1000.");
 	number=1000;
 } else {
 	let number = Number(_number);
 }
-console.log(number);
 
-
-
-	function prime_nums(){
+function prime_nums(){
 		console.log('Hola')
 	}
 
@@ -20,11 +18,13 @@ window.onload = function createBtn(){
 	console.log(number);
 	var sieve = document.getElementById("sieve");
 	for (var i = 1; i <= number; i++) {
-		var button = document.createElement("BUTTON");
-		button.textContent=(i);
-		sieve.appendChild(button);
+		var btn = document.createElement("button");
+		btn.textContent=(i);
+		btn.value=i;
+		sieve.appendChild(btn);
 		}
 	}
 
+	
 
 
