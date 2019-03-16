e71b94514fb5a9ff4fbfd233f1cb277daf3c6dca
Hector-HG <Hector-HG.github.com>
2019-03-15 22:53:23 -0600

    Update Interactividad con JS.js

--- Interactividad con JS.js
+++ Interactividad con JS.js
@@ -1,47 +1,73 @@
 const prime_numbers = 20;
 const max = 1000;
-let _number = prompt("Schreiben Sie eine Zahl, die gleich 20 oder größer ist",20);
+let _number = prompt("Schreiben Sie eine Zahl, die gleich 20 oder größer ist", 20);
 let number = Number(_number);
 let numC = [];
+var btnNup = document.getElementById("btnNp");
+var btnCl = document.getElementById("btnC");
 
-
-if (number>1000) {
+if (number > 1000) {
 	alert("Es ist viele, besser 1000.");
-	number=1000;
+	number = 1000;
 } else {
-	if (number<20){
+	if (number < 20) {
 		alert("Es ist reicht nicht, es werden 20.");
-		number=20;
-	}
-	else{
+		number = 20;
+	} else {
 		let number = Number(_number);
 	}
 }
 
-function prime_nums(){
-		console.log('Hola')
-	}
-
-window.onload = function createBtn(){
+window.onload = function createBtn() {
 	var sieve = document.getElementById("sieve");
-	for (var i = 1; i <= number; i++) {
+	for (i = 1; i <= number; i++) {
 		var btn = document.createElement("button");
-		btn.textContent=(i);
-		btn.value="p"+i;
-		btn.className="mult";
+		btn.textContent = (i);
+		btn.id = "p" + i;
+		btn.className = "mult";
 		sieve.appendChild(btn);
-		}
 	}
-	
+	for (i = 31; i > Math.sqrt(number); i--) {
+		btnu = document.getElementById("btn"+i);
+		remove = btnu.parentNode;
+		remove.removeChild(btnu);
+	}
+}
+
 function prime(nummer) {
-	for(i=1; i <= number; i++){
-		if(i % nummer == 0 && numC.includes(i) == false){
-			document.getElementById("p"+i).className="mult"+i;
+	for (i = 1; i <= number; i++) {
+		if (i % nummer == 0 && numC.includes(i) == false) {
+			document.getElementById("p" + i).className = "mult" + nummer+ " mult";
 			numC.push(i);
 		}
 	}
 }
 
+let count = 0;
+let primeNumbs = [];
+let currentN = 0;
 
-
-	
+btnNup.onclick = function () {
+	for (currentN = 2; currentN <= number; currentN++) {
+		for (j = 1; j <= currentN; j++) {
+			if (currentN % j == 0) {
+				count++;
+			}
+		}
+		if (count == 2 && primeNumbs.includes(currentN) == false) {
+			document.getElementById("p" + currentN).className = "mult multP";
+			primeNumbs.push(currentN);
+			numC.push(currentN);
+			count = 0;
+		} else if (count > 2) {
+			count = 0;
+		}
+	}
+}
+btnCl.onclick = function () {
+	for (i = 1; i <= number; i++) {
+		document.getElementById("p" + i).className = "mult";
+		primeNumbs.splice(0, number);
+		numC.splice(0, number);
+	}
+}
